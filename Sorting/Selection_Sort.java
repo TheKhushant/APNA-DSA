414526ca352849ff1d3acf1e2f9739968d1a141e
Khushant Wankhede <yashwankhede53@gmail.com>
2024-11-07 08:24:02 +0530

    Create Selection_Sort.java

---
+++ Sorting/Selection_Sort.java
@@ -0,0 +1,27 @@
+public class Selection{
+    public static void selecti(int arr[]){
+        for(int i=0; i<arr.length-1;i++){
+            int min=i;
+            for(int j = i+1; j<arr.length;j++){
+                if(arr[min] > arr[j]){
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        // return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,2,3,1,5};
+        selecti(arr);
+        Display(arr);
+    }
+    public static void Display(int[] arr) {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
